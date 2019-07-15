61e1aa8cd600e48b2ade9b163cfb0364bdf3c1a8
Nadir Tareen <nadirkt@uw.edu>
2019-07-14 17:09:06 -0700

    finished FiCamera

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.png';
 import './App.css';
 import TextInput from './TextInput.js';
 import NamePicker from './NamePicker.js';
+import firebase from 'firebase';
+import Camera from'react-snap-pic';
+
 
 
 class App extends React.Component {
@@ -10,7 +13,52 @@ class App extends React.Component {
   state={
     messages: [],
     name: "",
-    editName: false
+    editName: false,
+    showCamera: false
+  }
+
+  componentWillMount(){
+    var name = localStorage.getItem('name')
+    if(name){
+      this.setState({name})
+    }
+
+    /* <=========================> */
+    firebase.initializeApp({
+      apiKey: "AIzaSyAvULy-VFyDBOEbxnq5HeRsOU2bUAUiWi0",
+      authDomain: "hcde438project.firebaseapp.com",
+      databaseURL: "https://hcde438project.firebaseio.com",
+      projectId: "hcde438project",
+      storageBucket: "hcde438project.appspot.com",
+      messagingSenderId: "425594893380",
+      appId: "1:425594893380:web:29a6fe602b5005a9"
+    });
+    
+    this.db = firebase.firestore();
+
+    this.db.collection("messages").onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          //console.log(change.doc.data())
+          this.receive(change.doc.data())
+        }
+      })
+    })
+    /* <=========================> */
+  }
+
+  receive = (m) => {
+    const messages = [m, ...this.state.messages]
+    messages.sort((a,b)=>b.ts-a.ts)
+    this.setState({messages})
+  }
+
+  send = (m) => {
+    this.db.collection("messages").add({
+      ...m,
+      from: this.state.name || 'No name',
+      ts: Date.now()
+    })
   }
 
   gotMessage = (m) => {
@@ -19,6 +67,18 @@ class App extends React.Component {
       from: this.state.name
     }
     this.setState({messages: [message, ...this.state.messages]})
+  }
+
+  setEditName = (editName) => {
+    if(!editName) {
+      localStorage.setItem('name', this.state.name)
+    }
+    this.setState({editName});
+  }
+
+  takePicture = (img) => {
+    console.log(img)
+    this.setState({showCamera:false})
   }
 
   render() {
@@ -32,23 +92,34 @@ class App extends React.Component {
             <h1> Bolo</h1>
           </div>
           <NamePicker name={this.state.name} editName={this.state.editName} 
-          changeName={(n) => this.setState({name: n})} setEditName={(m) => this.setState({editName: m})}/>
+          changeName={(n) => this.setState({name: n})} setEditName={this.setEditName}/>
         </header>
         <main className="messages">
           {messages.map((m, i)=>{
             return (
-                <div key={i} className="bubble-wrap" style={(this.state.name !== m.from) ? {justifyContent: 'flex-end', marginRight: 10}: {}}>
-                  <div className="bubble" style={(this.state.name !== m.from) ? {background: '#cad7fe', color: 'white'}: {}}>
-                  <span className="bubble-name" style={(this.state.name !== m.from) ? {color: 'white'}: {}} >{m.from}</span>
-                    <span>{m.text}</span>
-                  </div>
-                </div>
+                <Message key={i} m={m} name={this.state.name} ></Message>
           )})}
         </main>
-        <TextInput sendMessage={this.gotMessage} />
+        {this.state.showCamera && 
+          <Camera takePicture={this.state.takePicture} />
+        }
+        <TextInput sendMessage={(text) => this.send({text})} showCamera={() => this.send.showCamera = true}/>
       </div>
     );
   }
 }
 
 export default App;
+
+function Message(props) {
+  var {m, name} = props;
+  return (
+    <div className="bubble-wrap" from={(name === m.from) ? 'me': 'you'}>
+      <div className="bubble" >
+      <span className="bubble-name" >{m.from}</span>
+        <span>{m.text}</span>
+      </div>
+    </div>
+  )
+}
+
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiSend } from 'react-icons/fi';
+import { FiCamera } from 'react-icons/fi';
 
 class TextInput extends React.Component {
 
@@ -22,13 +23,16 @@ class TextInput extends React.Component {
     render() {
         return (
         <div className ="text-input">
+            <button className="camera" onClick={this.showCamera}>
+                <FiCamera style={{height:15, width:15, color:0}} />
+            </button>
             <input value={this.state.text} 
                 onChange={e=> this.setState({text: e.target.value})}
                 onKeyPress={this.keyPress}
             />
             <button disabled={!this.state.text}
                 onClick={this.send}>
-                <FiSend style={{height:15, width:15, color: 0}} />
+                <FiSend style={{height:15, width:15, color:0}} />
             </button>
         </div>
         );
